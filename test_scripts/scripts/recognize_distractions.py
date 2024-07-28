a44a2d87f21bdaeeeb5f3df2fce64752c1611571
Emma <emmascinti3@gmail.com>
2024-07-28 20:27:49 +0200

    added distance from camera

--- test_scripts/scripts/recognize_distractions.py
+++ test_scripts/scripts/recognize_distractions.py
@@ -5,7 +5,11 @@ import mediapipe as mp
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-image_path = '/home/vboxuser/Desktop/Smart-Robotics-Project/test_scripts/3quarti.jpeg'
+focal_length = 800  # Lunghezza focale in pixel
+real_height = 1.7  # Altezza media della persona in metri
+
+
+image_path = '/home/vboxuser/Desktop/Smart-Robotics-Project/test_scripts/2_people.png'
 image = cv2.imread(image_path)
 
 if image is None:
@@ -19,6 +23,9 @@ person_detections = detections[detections['name'] == 'person']
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+
+def calculate_distance(bbox_height):
+    return (real_height * focal_length) / bbox_height
 
 def get_head_orientation(landmarks):
     nose_tip = landmarks[0]
@@ -45,10 +52,16 @@ def is_attentive(face_vector, left_right_vector):
 def put_text_on_image(image, text, position, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, color=(0, 255, 0), thickness=2):
     cv2.putText(image, text, position, font, font_scale, color, thickness, cv2.LINE_AA)
 
+
 with mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5) as pose:
     for index, person in person_detections.iterrows():
         xmin, ymin, xmax, ymax = int(person['xmin']), int(person['ymin']), int(person['xmax']), int(person['ymax'])
         
+        bbox_height= ymax-ymin
+        distance = calculate_distance(bbox_height)
+
+        font_scale = bbox_height / 700
+
         person_roi = image[ymin:ymax, xmin:xmax]
         rgb_roi = cv2.cvtColor(person_roi, cv2.COLOR_BGR2RGB)
         rgb_roi.flags.writeable = False
@@ -71,8 +84,13 @@ with mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5) as pose:
             attentive = is_attentive(face_vector, left_right_vector)
             
             label = "Attentive" if attentive else "Distracted"
-            put_text_on_image(image, label, (xmin, ymin - 10), font_scale=0.5, thickness=1)
-
+            put_text_on_image(image, label, (xmin, ymin - 10), font_scale=font_scale, thickness=1)
+        
+        text_position = (xmin, ymin - 20)
+        if text_position[1] < 0:  
+            text_position = (xmin, ymin + 20)
+                
+        put_text_on_image(image, f'Distance: {distance:.2f}m', text_position, font_scale=font_scale, thickness=1)        
         image[ymin:ymax, xmin:xmax] = bgr_roi
 
 output_path = '/home/vboxuser/Desktop/Smart-Robotics-Project/test_scripts/output_image.jpg'
