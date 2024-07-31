19a9e9c1342f8cb9ad67dad49e46b69228993ec9
Stefano <poli14.sp@gmail.com>
2024-07-31 22:51:02 +0200

    camera publisher & subscriber fully working

--- pedestrian_detection/src/detect_people/scripts/camera_publisher.py
+++ pedestrian_detection/src/detect_people/scripts/camera_publisher.py
@@ -1,33 +1,34 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env python 
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+import os
+from pathlib import Path
+root_path,_=str(Path(__file__).resolve()).split("/Smart-Robotics-Project")
+frames_path=root_path+"/Smart-Robotics-Project/show_frame/frames"
 
 def publish_frame():
-    rospy.init_node("camera_publisher", anonymous=True)
-    publisher=rospy.Publisher("video_topic",Image,queue_size=60)
-    rate=rospy.Rate(1) #3 images per seconds
+    rospy.init_node("camera_publisher_from_kairos", anonymous=True)
+    publisher=rospy.Publisher("frame_topic",Image,queue_size=60)
+    rate=rospy.Rate(10) #3 images per seconds
 
-    capture=cv2.VideoCapture(0)
-    if capture.isOpened()==False:
-        rospy.logerr("Camera Not found")
     bridge=CvBridge()
 
     while not rospy.is_shutdown():
-        ret,cap=capture.read()
-        if ret:
-            try:
-                image=bridge.cv2_to_imgmsg(cap)
-                publisher.publish(image)
-                rospy.loginfo("Video frame captured and published")
-            except Exception as e:
-                rospy.logerr(e)
+        frames=os.listdir(frames_path)
+        frames=[os.path.join(frames_path,frame) for frame in frames]
+
+        for frame in frames:
+            cv_image=cv2.imread(frame)
+            if cv_image is not None:
+                image_msg=bridge.cv2_to_imgmsg(cv_image,encoding="bgr8")
+                publisher.publish(image_msg)
+                rospy.loginfo(f"Image Published: {frame}")
         
         rate.sleep()
 
-    capture.release()
+    
     
 if __name__ == '__main__':
     try:
